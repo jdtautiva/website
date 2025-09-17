faac2b7414f1b578cc49db427ab54cd82c4fe237
jdtautiva <jdtautiva@gmail.com>
2025-09-17 15:23:47 +0000

    Deploying to gh-pages from @ jdtautiva/website@5328dfb385a3367fbb80edb4147c840c8f101d0f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -53,7 +53,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "members of the lab or group",
+          description: "Research Groups",
           section: "Navigation",
           handler: () => {
             window.location.href = "/website/people/";
