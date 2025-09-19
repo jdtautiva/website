eed74ea6f789b207e1c1e6a9f935c317f3ebe856
jdtautiva <jdtautiva@gmail.com>
2025-09-19 16:01:34 +0000

    Deploying to gh-pages from @ jdtautiva/website@9402176a6f5400ee1b2eaeab98a56400302d2c43 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -109,16 +109,6 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/website/projects/4_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/website/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/website/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
