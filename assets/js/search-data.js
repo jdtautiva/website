ef1c221bd64c2d7e40d9da9aa9a06087695e1370
jdtautiva <jdtautiva@gmail.com>
2025-09-19 15:30:57 +0000

    Deploying to gh-pages from @ jdtautiva/website@d11235cad440bb418f9f96dbcbd5d9a411433234 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -194,6 +194,17 @@ ninja.data = [{
             window.location.href = "/website/blog/2024/chartjs/";
           
         },
+      },{id: "post-a-post-with-tikzjax",
+        
+          title: "a post with TikZJax",
+        
+        description: "this is what included TikZ code could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/website/blog/2023/tikzjax/";
+          
+        },
       },{id: "post-a-post-with-jupyter-notebook",
         
           title: "a post with jupyter notebook",
