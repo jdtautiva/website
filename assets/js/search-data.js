8ca32d41b7e93e522d03cb3b403be0347ab74236
jdtautiva <jdtautiva@gmail.com>
2025-10-05 22:10:04 +0000

    Deploying to gh-pages from @ jdtautiva/website@e83f14af54fc6167ef6dc7eec61b0678a9c5566f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -161,9 +161,9 @@ ninja.data = [{
           description: "Recent Trends and Global Discussions",
           section: "Projects",handler: () => {
               window.location.href = "/website/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-climate-change-and-economic-behavior",
+          title: 'Climate Change and Economic Behavior',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/website/projects/3_project/";
             },},{
